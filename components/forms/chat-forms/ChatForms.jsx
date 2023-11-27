1dbcb94ea2160cee00c6b4cfda139b28ee50e818
abubakar471 <abdurjoy2001@gmail.com>
2023-11-27 07:10:45 +0600

    initial loader file added

--- components/forms/chat-forms/ChatForms.jsx
+++ components/forms/chat-forms/ChatForms.jsx
@@ -5,11 +5,13 @@ import { Send } from "lucide-react"
 const ChatForms = ({ currentUser, prompt, setPrompt, isGenerating, handleSubmit }) => {
 
     return (
-        
-        <form onSubmit={handleSubmit} className="flex items-center gap-x-2 w-full my-4 bg-white shadow-lg rounded-sm px-2">
-            <input type="text" placeholder="Message ChatGPT4Job" disabled={isGenerating} value={prompt} onChange={(e) => setPrompt(e.target.value)} className="grow p-4 outline-none" />
-            <button type="submit" disabled={isGenerating}><Send /></button>
-        </form>
+        <div className="flex flex-col items-center justify-center gap-y-2 w-full">
+            <form onSubmit={handleSubmit} className="flex items-center gap-x-2 w-full my-4 bg-white shadow-lg rounded-sm px-2">
+                <input type="text" placeholder="Message ChatGPT4Job" disabled={isGenerating} value={prompt} onChange={(e) => setPrompt(e.target.value)} className="grow p-4 outline-none" autoFocus />
+                <button type="submit" disabled={isGenerating}><Send /></button>
+            </form>
+            <p className="text-sm text-gray-600 text-center pb-2">ChatGPT4Job can make mistakes. Consider checking important information.</p>
+        </div>
     )
 }
 
